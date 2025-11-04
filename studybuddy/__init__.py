1ec928c2beb5f9228e92854c793a1d78603eb042
Jeanmarck12 <jrc9921@nyu.edu>
2025-11-03 23:31:24 -0500

    Expand __all__ exports in __init__.py

--- studybuddy/__init__.py
+++ studybuddy/__init__.py
@@ -16,5 +16,22 @@ from .core import (
     list_reasons,
     list_vibes,
 )
-__all__ = ["study_tip", "motivate", "excuse", "study_plan"]
+
+__all__ = [
+    "study_tip",
+    "motivate",
+    "excuse",
+    "study_plan",
+    "roast",
+    "compliment",
+    "break_tip",
+    "pomodoro_plan",
+    "playlist",
+    "secret",
+    "list_topics",
+    "list_styles",
+    "list_reasons",
+    "list_vibes",
+]
+
 __version__ = "0.1.0"
